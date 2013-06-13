0c40edf68488b1763534b7270b0b6afa2427ab38
Sergio E. Tobal <set.tobur@gmail.com>
2013-06-13 22:30:26 +0200

    Answers in file(Read/Writer), and choose only one String from the array

--- src/localhost/team2/magic8ball/MainActivity.java
+++ src/localhost/team2/magic8ball/MainActivity.java
@@ -1,5 +1,12 @@
 package localhost.team2.magic8ball;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -9,15 +16,7 @@ import android.widget.TextView;
 public class MainActivity extends Activity {
 
 	// To-do: move answers to file for reading
-	public String[] answers = { "It is certain", "It is decidely so",
-			"Without a doubt", "Yes definitely", "You may rely on it",
-			"As I see it yes", "Most likely", "Outlook good", "Yes",
-			"Signs point to yes", "Reply hazy try again", "Ask again later",
-			"Better not tell you now", "Concentrate and ask again",
-			"Don\'t count on it", "My reply is no", "My sources say no",
-			"Outlook not so good", "Very doubtful" };
-
-	TextView tviewEightBallAns;
+	private TextView tviewEightBallAns;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -38,8 +37,50 @@ public class MainActivity extends Activity {
 
 	// To-do: implement shake using sensor
 	public void onShake() {
-		int answer = (int) (Math.random() * answers.length); // generate random number/index for eight ball answer
-		tviewEightBallAns.setText(answers[answer]);
+		tviewEightBallAns.setText(createFileAnswers());
 	}
 
+	private String createFileAnswers() {
+		File fileAnswers = new File(this.getFilesDir(), "FileAnswers");
+
+		try {
+			if (!fileAnswers.exists()){//Only write the file the first time
+				writeFile(fileAnswers);
+			}
+			return readFile(fileAnswers);
+
+		} catch (Exception e) {
+			System.out.println(e.getStackTrace());
+		}
+		return null;
+	}
+
+	private static String readFile(File fileAns) throws IOException {
+		BufferedReader bfReader = new BufferedReader(new InputStreamReader(new FileInputStream(fileAns)));
+		int answer = (int) (Math.random() * 20);
+		String line = null;
+		for (int i = 0; i <= answer; i++) {
+			line = bfReader.readLine();
+		}
+		bfReader.close();
+		return line;
+		
+	}
+
+	private static void writeFile(File fileAns) throws IOException {
+		PrintWriter writer = new PrintWriter(fileAns);
+		String[] answers = { "It is certain", "It is decidely so",
+				"Without a doubt", "Yes definitely", "You may rely on it",
+				"As I see it yes", "Most likely", "Outlook good", "Yes",
+				"Signs point to yes", "Reply hazy try again", "Ask again later",
+				"Better not tell you now", "Concentrate and ask again",
+				"Don\'t count on it", "My reply is no", "My sources say no",
+				"Outlook not so good", "Very doubtful", "No" };
+
+		for (String oneAns : answers) {//for each string in the array I create a string oneAns
+			writer.write(oneAns+"\n");
+		}
+		writer.flush();
+		writer.close();
+	}
 }
