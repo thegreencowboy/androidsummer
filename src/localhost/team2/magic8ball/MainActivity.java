ed9852d41906735701e78f0df4fdebb92be7e23e
Sergio E. Tobal <set.tobur@gmail.com>
2013-06-15 01:28:19 +0200

    App working

--- src/localhost/team2/magic8ball/MainActivity.java
+++ src/localhost/team2/magic8ball/MainActivity.java
@@ -2,27 +2,34 @@ package localhost.team2.magic8ball;
 
 import android.app.Activity;
 import android.content.res.Resources;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.Window;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements SensorEventListener{
 
 	private TextView tviewEightBallAns;
 	private String[] answers;
 	
+	private SensorManager sensorManager;
+	  
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE); // hide main activity title
 		setContentView(R.layout.activity_main);
-		
-		Resources res = getResources();
-		answers = res.getStringArray(R.array.string_array_answers);
+
+		answers = getResources().getStringArray(R.array.string_array_answers);
 		tviewEightBallAns = (TextView) findViewById(R.id.tview_eight_ball_answer);
 		
-		onShake();
+		sensorManager=(SensorManager)getSystemService(SENSOR_SERVICE);
+		sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+		  
 	}
 
 	@Override
@@ -32,16 +39,45 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
-		// TODO implement shake detection using sensor
+	// TODO implement shake detection using sensor
 	public void onShake() {
 		// shake detected, so change text to answer
 		tviewEightBallAns.setText(getAnswer());
 	}
-	
+
 	public String getAnswer() {
 		int answerIndex = (int)(Math.random() * answers.length);
 		String answer = answers[answerIndex];
 		return answer;
 	}
 
+	@Override
+	public void onSensorChanged(SensorEvent event) {
+		float x = event.values[0];
+        float y = event.values[1];
+        float z = event.values[2];
+		
+		if(event.sensor.getType()==Sensor.TYPE_ACCELEROMETER){
+			double mAccelCurrent = Math.sqrt((double) (x*x + y*y + z*z));
+            if (mAccelCurrent > 10) {
+                 onShake();
+            }
+		}
+	}
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+	}
+	
+	@Override
+	protected void onPause() {
+		super.onPause();
+		sensorManager.unregisterListener(this);
+	}
 }
